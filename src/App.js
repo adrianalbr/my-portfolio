ff253e3f7b46a8a717cb83c5147fd0716940d8f3
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-01-26 19:08:19 -0500

    created containers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Contact from "./containers/Contact/Contact";
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+       <Switch>
+         <Route path="/Contact" component={Contact}/>
+         <Route path="/Portfolio" component={Portfolio}/>
+         <Route path="/" component={Home}/>
+
+
+       </Switch>
+     </Router>
     </div>
   );
 }
