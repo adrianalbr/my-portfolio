83147899d9ae10bc88c792afa29913ab154a78ff
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-01-29 18:50:29 -0500

    added container for about me and photos

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            
-
+            <main>
+            <div className="container about-me">
+                 <div className="row">
+                    <div className="col-md-9 main-body-contain">
+                        <h1>About Adriana</h1>
+                        
+                        <div class="container-xs">
+                        <img src="src/assets/me.jpeg" class="rounded float-sm-left img-responsive" alt="Adriana" width="200"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </main>
         </div>
     );
 };
